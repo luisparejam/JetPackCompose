4e9dc1a55ea0b2f23ef68b14c5aae1a1e71254cb
Luis Pareja <lpareja@hotmail.com>
2025-03-30 19:55:14 -0400

    video 38

--- app/src/main/java/com/example/jetpackpruebas/MainActivity.kt
+++ app/src/main/java/com/example/jetpackpruebas/MainActivity.kt
@@ -4,18 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackpruebas.ui.theme.JetPackPruebasTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +29,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MiPrimerComposable()
+            MiSegundoComposable()
         }
     }
 }
@@ -41,4 +47,31 @@ fun MiPrimerComposable(){
         Text(text="Hola alumnos de Kotlin")
         Text(text="Este es nuestro primer composable")
     }
+}
+
+@Composable
+fun MiSegundoComposable(){
+    Box(modifier=Modifier.fillMaxSize().padding(16.dp)){
+        /* Text(text="Hola arriba izquierda", modifier=Modifier.align(Alignment.TopStart))
+        Text(text="Hola centrado", modifier=Modifier.align(Alignment.Center))
+        Text(text="Hola abajo derecha", modifier=Modifier.align(Alignment.BottomEnd)) */
+
+        Image(
+            painter = painterResource(id = R.drawable.demo),
+            modifier = Modifier.align(Alignment.Center).fillMaxSize(),
+            contentDescription = "Coca-Cola"
+        )
+
+        Text(text = "Coca-Cola"
+            , fontSize = 24.sp
+            , color = Color.Red
+            , textAlign = TextAlign.Center
+            , modifier = Modifier.align(Alignment.Center))
+    }
+}
+
+@Preview
+@Composable
+fun MiSegundoComposablePreview(){
+    MiSegundoComposable()
 }
